49d374c4c34f740a2ca46903babf0ae30995f330
PervDaniil <studentdan123.900@gmail.com>
2025-05-06 18:46:55 +0600

    Fonts added in global.css

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <>
-      Hello World!
-    </>
+    <section className="min-h-screen w-full">
+      <div className="p-5 lg:p-20 flex flex-col flex-wrap">
+        <h1 className="text-7xl lg:text-9xl">Brain Center</h1>
+        <p>Explore our education platform "Brain Center"</p>
+      </div>
+    </section>
   );
 }
