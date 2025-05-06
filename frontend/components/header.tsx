8b9fcab6944bcbce45c819f2e3ced81a19bf9c90
PervDaniil <studentdan123.900@gmail.com>
2025-05-06 22:54:06 +0600

    Header component changed

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
     const sections = [
-        "Home", "FAQ", "Dashboard", "Contacts", "Info",
+        "Home", "FAQ", "Contacts", "Info", "Courses", "Dashboard",
       ]
       
     return (
         <header className="w-full backdrop-blur-md shadow-2xs">
             <div className="py-8 px-10 flex items-center">
                 <p className="font-[500]">Brain Center</p>
-                <nav className="flex-1 flex justify-evenly items-center">
+                <nav className="flex-1 flex items-center justify-center space-x-32">
                     {sections.map((section, index) => (
                         <ol key={index}>
                             <li><p className="font-[200]">{section}</p></li>
